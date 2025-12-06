1bee77883f2c1003b9c59d5f7a112390fe32d005
Abdelfattah Ragab <faaf.dv@gmail.com>
2025-12-06 23:12:11 +0200

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Wihngo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET: api/<AuthController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Successful";
+        }
+
+        // POST api/<AuthController>
+        [HttpPost]
+        public void Post([FromBody] Object loginDto)
+        {
+            Console.WriteLine(loginDto);
+        }
+    }
+}
